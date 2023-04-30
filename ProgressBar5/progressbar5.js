7dea731b98867f2a3731f4a70b699b341f852170
Rachel Fothergill <Fothergill-R@ulster.ac.uk>
2023-05-01 00:53:26 +0100

    Code commented and formatted

--- ProgressBar5/progressbar5.js
+++ ProgressBar5/progressbar5.js
@@ -1,18 +1,27 @@
+// Wait for the DOM to be fully loaded before executing the code
 document.addEventListener('DOMContentLoaded', function() {
+
+  // Get the element with the class 'progress-bar-fill'
   const progressBarFill = document.querySelector('.progress-bar-fill');
 
+  // Define a function that animates the progress bar to a given progress value
   function animateProgressBar(progress) {
-      let currentProgress = 0;
-      const progressInterval = setInterval(function() {
-          if (currentProgress >= progress) {
-              clearInterval(progressInterval);
-          } else {
-              currentProgress++;
-              progressBarFill.style.width = `${currentProgress}%`;
-          }
-      }, 20); // You can adjust this value to make the animation faster or slower
+    let currentProgress = 0;
+
+    // Set up an interval that runs every 20 milliseconds
+    const progressInterval = setInterval(function() {
+
+      // If the current progress is greater than or equal to the target progress, clear the interval
+      if (currentProgress >= progress) {
+        clearInterval(progressInterval);
+      } else {
+        // Otherwise, increment the current progress and update the width of the progress bar
+        currentProgress++;
+        progressBarFill.style.width = `${currentProgress}%`;
+      }
+      }, 20); //Animation Speed
   }
 
-  animateProgressBar(100); // shows 100% progress
-
+  // Call the animateProgressBar function with a progress value of 100, which will show 100% progress
+  animateProgressBar(100);
 });
