3ec113e109daf3fbc7de615afdfc9760145ffe87
Rachel Fothergill <Fothergill-R@ulster.ac.uk>
2023-05-01 01:27:35 +0100

    Java update

--- CostBenAnTool/CBTool.js
+++ CostBenAnTool/CBTool.js
@@ -1,39 +1,50 @@
-const costBenefitForm = document.getElementById('cost-benefit-form');
-const resultSection = document.getElementById('result-section');
+// Select the form and result section elements from the HTML
+const form = document.getElementById("cost-benefit-form");
+const resultSection = document.getElementById("result-section");
 
-costBenefitForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const initialCosts = parseFloat(document.getElementById('initial-costs').value);
-  const recurringCosts = parseFloat(document.getElementById('recurring-costs').value);
-  const opportunityCosts = parseFloat(document.getElementById('opportunity-costs').value) || 0;
-  const revenueGains = parseFloat(document.getElementById('revenue-gains').value);
-  const costSavings = parseFloat(document.getElementById('cost-savings').value);
-  const nonFinancialBenefits = document.getElementById('non-financial-benefits').value;
-  const implementationTime = parseFloat(document.getElementById('implementation-time').value);
-  const lifespan = parseFloat(document.getElementById('lifespan').value);
-  const discountRate = parseFloat(document.getElementById('discount-rate').value) / 100;
+// Attach an event listener to the form for when it's submitted
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
 
-  const netPresentValue = calculateNPV(initialCosts, recurringCosts, opportunityCosts, revenueGains, costSavings, implementationTime, lifespan, discountRate);
+  // Get the input values from the form
+  const initialCosts = parseFloat(document.getElementById("initial-costs").value);
+  const recurringCosts = parseFloat(document.getElementById("recurring-costs").value);
+  const opportunityCosts = parseFloat(document.getElementById("opportunity-costs").value) || 0;
+  const revenueGains = parseFloat(document.getElementById("revenue-gains").value);
+  const costSavings = parseFloat(document.getElementById("cost-savings").value);
+  const nonFinancialBenefits = document.getElementById("non-financial-benefits").value;
+  const implementationTime = parseInt(document.getElementById("implementation-time").value);
+  const lifespan = parseInt(document.getElementById("lifespan").value);
+  const discountRate = parseFloat(document.getElementById("discount-rate").value) / 100;
 
-  displayResult(netPresentValue, nonFinancialBenefits);
+  // Calculate the net present value
+  const npv = calculateNPV(initialCosts, recurringCosts, opportunityCosts, revenueGains, costSavings, implementationTime, lifespan, discountRate);
+
+  // Display the result on the page
+  displayResult(npv, nonFinancialBenefits);
 });
 
+// Calculate the net present value based on the input values
 function calculateNPV(initialCosts, recurringCosts, opportunityCosts, revenueGains, costSavings, implementationTime, lifespan, discountRate) {
-  let npv = -initialCosts;
+  let presentValue = -initialCosts; // Subtract initial costs from present value
   for (let i = 1; i <= lifespan * 12; i++) {
+    let cashFlow = costSavings + revenueGains;
     if (i <= implementationTime) {
-      npv += (revenueGains - costSavings - recurringCosts - opportunityCosts) / Math.pow(1 + discountRate, i / 12);
+      cashFlow -= recurringCosts + opportunityCosts;
     } else {
-      npv += (revenueGains - costSavings - recurringCosts) / Math.pow(1 + discountRate, i / 12);
+      cashFlow -= recurringCosts;
     }
+    let discountedCashFlow = cashFlow / Math.pow(1 + discountRate, i / 12);
+    presentValue += discountedCashFlow;
   }
-  return npv.toFixed(2);
+  return presentValue.toFixed(2);
 }
 
+// Display the net present value on the page
 function displayResult(npv, nonFinancialBenefits) {
-  let resultText = `Net Present Value: $${npv}`;
+  let resultText = `<p>The calculated NPV is $${npv}.</p>`;
   if (nonFinancialBenefits) {
-    resultText += `\n\nNon-Financial Benefits:\n${nonFinancialBenefits}`;
+    resultText += `<p>Non-financial benefits: ${nonFinancialBenefits}</p>`;
   }
-  resultSection.textContent = resultText;
+  resultSection.innerHTML = resultText;
 }
